ab7ac04f00108e0c8f13c4c79a6b646cc4b75ba6
horyujey <jeyjeyjeey@gmail.com>
2022-09-20 22:20:14 +0900

    chore: delete sample data

--- apps/anote/pages/data.tsx
+++ apps/anote/pages/data.tsx
@@ -31,36 +31,3 @@ export const annotationDefault: annotation = {
     isInstruction: false,
     isAutonomy: false
   }
-
-export const jsonData = [
-    {
-        "id": "LCC.CjgKDQoLQjBaTWR5SWxoSjQqJwoYVUNMTzlRRHhWTDRibnZSUnN6Nks0YnNREgtCMFpNZHlJbGhKNBI6ChpDSWVhbkpXNXRfa0NGV3NWclFZZEptNExqdxIcQ09xYmo4Q3p0X2tDRlQ5STlRVWRiOUlDTlExMw",
-        "snippet": {
-                "messageText": "フラグ立てないでえええ:_ちーもじなきちー::_ちーもじKusa:"
-        },
-        "authorDetails": {
-            "channelId": "UCF1LK5-hQMT50mOh_t6d6tw",
-            "displayName": "わさび餅",
-            "profileImageUrl": "https://yt3.ggpht.com/8ha5rgkWL5e-FDeuIPuA3n3dggRPtBOhfw1frVTNTS3RayBym9b2kHsseUcGE8bkoIhSxUtsaQ=s88-c-k-c0x00ffffff-no-rj",
-            "isVerified": false,
-            "isChatOwner": false,
-            "isChatSponsor": true,
-            "isChatModerator": false
-        }
-    },
-    {
-        "id": "LCC.CjgKDQoLQjBaTWR5SWxoSjQqJwoYVUNMTzlRRHhWTDRibnZSUnN6Nks0YnNREgtCMFpNZHlJbGhKNBI6ChpDSWVhbkpXNXRfa0NGV3NWclFZZEptNExqdxIcQ09xYmo4Q3p0X2tDRlQ5STlRVWRiOUlDTlExMwTIGAUID",
-        "snippet": {
-                "messageText": "フラグ立てないでえええ:_ちーもじなきちー::_ちーもじKusa:"
-        },
-        "authorDetails": {
-            "channelId": "UCF1LK5-hQMT50mOh_t6d6tw",
-            "displayName": "わさび餅",
-            "profileImageUrl": "https://yt3.ggpht.com/8ha5rgkWL5e-FDeuIPuA3n3dggRPtBOhfw1frVTNTS3RayBym9b2kHsseUcGE8bkoIhSxUtsaQ=s88-c-k-c0x00ffffff-no-rj",
-            "isVerified": false,
-            "isChatOwner": false,
-            "isChatSponsor": true,
-            "isChatModerator": true
-        }
-    }
-]    
